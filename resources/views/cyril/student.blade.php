49a6730337a3af11cfe398d7b4954693d901fd0c
COdiwuor <cyrilondime@gmail.com>
2018-07-06 00:00:50 +0300

    Update student.blade.php

--- resources/views/cyril/student.blade.php
+++ resources/views/cyril/student.blade.php
@@ -2,6 +2,7 @@
 <h3> Insert New Student </h3>
 <a href="/studenttable">Student Table</a>
 
+<div class="container">
     <form method="post" action ="/studentstore" encytype = "multipart/form-data">
         {{csrf_field()}}
         <label>Student Number :</label> <input type ="text"  name="Student_Number"  value="{{$student->student_number or ''}}" required>
@@ -16,5 +17,42 @@
          <br>
          
          <button type="submit" name ="Save">save</button>
+         
     </form>
+    </div>
+    <style>
+    input[type=text], select, textarea {
+    width: 100%; /* Full width */
+    padding: 12px; /* Some padding */ 
+    border: 1px solid #ccc; /* Gray border */
+    border-radius: 4px; /* Rounded borders */
+    box-sizing: border-box; /* Make sure that padding and width stays in place */
+    margin-top: 6px; /* Add a top margin */
+    margin-bottom: 16px; /* Bottom margin */
+    resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+}
+
+/* Style the submit button with a specific background color etc */
+input[type=submit] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+/* When moving the mouse over the submit button, add a darker green color */
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+/* Add a background color and some padding around the form */
+.container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+    </style>
+    
     </html>
